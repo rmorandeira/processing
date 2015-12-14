7f2deb2d2437a935ccd704ad05f2dbe6c96f9d5d
Roi <rmorandeira@gmail.com>
2015-12-14 17:00:39 +0100

    Changes

--- nonLinearHistory/sketch.js
+++ nonLinearHistory/sketch.js
@@ -1,4 +1,5 @@
-var action  = "";
+
+var imgBackground;
 var imgStep1;
 var imgStep2;
 var imgStep3;
@@ -7,22 +8,12 @@ var imgStep5;
 var imgStep6;
 var imgWrong;
 
-//Step 1 completion
-var step1 = false;
-//Step 2 completion
-var step2 = false;
-//Step 3 completion
-var step3 = false;
-//Step 3 completion
-var step3 = false;
-//Step 4 completion
-var step4 = false;
-//Step 5 completion
-var step5 = false;
-//Step 6 completion
-var step6 = false;
+//initialStep
+var step;
+var action="";
 
-function preload(){
+function preload() {
+  imgBackground = loadImage("/img/background.jpg");
   imgStep1 = loadImage("/img/step1.jpg");
   imgStep2 = loadImage("/img/step2.jpg");
   imgStep3 = loadImage("/img/step3.jpg");
@@ -30,113 +21,121 @@ function preload(){
   imgStep5 = loadImage("/img/step5.jpg");
   imgStep6 = loadImage("/img/step6.jpg");
   imgWrong = loadImage("/img/wrong.jpg");
-  
+
 }
 
 function setup() {
-  createCanvas (300,300);
+  createCanvas(300, 300);
   textFont("Helvetica");
   textSize("22");
-  textAlign(CENTER);
-  image(imgStep1,0,0);
+  //textAlign(LEFT);
 }
-function keyPressed (){
-  if(keyCode == BACKSPACE){
-    action = "";
+function keyPressed() {
+  if (keyCode==BACKSPACE) {
+    action="";
   }
 }
-  
-function keyTyped (){
-  
-  action += key;
-  
-}
+
 function draw() {
-   
-  
-   switch(action){
-    case 'left':
-      /*It's just a jump to the left.*/
-      image(imgStep2,0,0);
-      step1 = true;
-      step2 = false;
-      step3 = false;
-      step4 = false;
-      step5 = false;
-      step6 = false;
-      action = "";
-    break;
-    case 'right':
-      /*And then a step to the right.*/
-      if(step1){
-         step1 = false;
-         step2 = true;
-         step3 = false;
-         step4 = false;
-         step5 = false;
-         step6 = false;
-         image(imgStep3,0,0);
-         action = "";
-      }
-    break;
-    case 'hips':
-      /*With your hand on your hips.*/
-       if(step2){
-         step1 = false;
-         step2 = false;
-         step3 = true;
-         step4 = false;
-         step5 = false;
-         step6 = false;
-         image(imgStep4,0,0);
-         action = "";
-      }
-    break;
-    case 'knees':
-      /*You bring your knees in tight*/
-      if(step3){
-         step1 = false;
-         step2 = false;
-         step3 = false;
-         step4 = true;
-         step5 = false;
-         step6 = false;
-         image(imgStep5,0,0);
-         action = "";
-      }
-    break;
-    case 'tight':
-      /*But it's the pelvic thrust*/
-        if(step4){
-         step1 = false;
-         step2 = false;
-         step3 = false;
-         step4 = false;
-         step5 = true;
-         step6 = false;
-         image(imgStep6,0,0);
-         action = "";
-      }
-    break;
-    case 'timeWarp':
-      /*Let's do the Time Warp again*/
-      if(step6){
-         step1 = false;
-         step2 = false;
-         step3 = false;
-         step4 = false;
-         step5 = false;
-         step6 = true;
-         image(imgStep6,0,0);
-         action = "";
-      
+  switch(step){
+      case 0:
+      initialStep();
+      break;
+      case 1:
+      jumpLeft();
+      break;
+      case 2:
+      stepRight();
+      break;
+      case 3:
+      handHips();
+      break;
+      case 4:
+      pelvicThrust();
+      break;
+      case 5:
+      initialStep();
+      break;
+      default:
+      instructions();
+      break;
+  } 
+  textSize("22");
+  text(action, width/2, 252);
+}
+
+function keyTyped() {
+  print("STEP:" + step + " ACTION:" + action);
+  if(keyCode== RETURN){
+    if(action=='instructions' || 'help' || '?'){
+      instructions();
     }
-    break;
-    default:
-      /*Oh Oh,Wrong move*/
-      //image(imgWrong,0,0);
-    break;
+    if(action=="start"){
+      initialStep();
+    }
+    if(action=='left' && step == 0){
+        jumpLeft();
+    }
+    if(action=='right' && step== 1){
+        stepRight();
+    }
+    if(action=='hips' &&  step==2){
+        handHips();
+    }
+    if(action=='tight' &&  step==3){
+        kneesTight();
+    }
+    if(action=='thrust'  &&  step==4){
+        pelvicThrust();
+    }
+    if(action=='again'  &&  step==5){
+        initialStep();
+    }
+    action="";
+  }else{
+     action += key;
+
   }
-    
-  text(action,0 ,222,width,30);
+}
+function instructions(){
+  image(imgBackground,0,0);
+  textSize("10");
+  fill(0,0,0);
+  text("Ready for the Time Warp Dance?¿",11,51);
+  text("Just type the BOLD text for the next step",11,71);
+  text("Type start now! And Start the Time Warp Dance!!",11,91);
+  text("If you need help, just type 'instructions','help' or '?'",11,111);
+  fill(255,124,0);
+  text("Ready for the Time Warp Dance?¿",10,50);
+  text("Just type the BOLD text for the next step",10,70);
+  text("Type start now! And Start the Time Warp Dance!!",10,90);
+  text("If you need help, just type 'instructions','help' or '?'",10,110);
+}
+
+function initialStep() {
+   step=0;
+  image(imgStep1, 0, 0);
+}
+function jumpLeft() {
+  step=1;
+  image(imgStep2, 0, 0);
+}
+function stepRight() {
+  step=2;
+  image(imgStep3, 0, 0);
+}
+function handHips() {
+  step=3;
+  image(imgStep4, 0, 0);
+}
+function kneesTight() {
+  step=4;
+  image(imgStep5, 0, 0);
+}
+function pelvicThrust() {
+  step=5;
+  image(imgStep6, 0, 0);
+}
+function error() {
+  image(imgWrong, 0, 0);
 }
